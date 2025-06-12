0cb156f95d60e8095130d6a5486edc9b524f61b5
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-12 15:30:21 +0700

    membuat case 3 untuk mengerjakan operasi display

--- Queues.cpp
+++ Queues.cpp
@@ -129,3 +129,7 @@ int main() {
                     q.remove();
                     break;
                 }
+                case '3': {
+                    q.display();
+                    break;
+                }
