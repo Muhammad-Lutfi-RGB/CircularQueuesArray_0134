718a254605176c765f3dabd6aeb56e9bffad3016
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-12 15:52:02 +0700

    membuat default dan catch

--- Queues.cpp
+++ Queues.cpp
@@ -136,5 +136,14 @@ int main() {
                 case '4' : {
                     return 0;
                 }
-
+                default: {
+                    cout << "Invalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception &e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
 }
